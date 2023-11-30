c795bdf592792a82e61154b3c2d6d52e3450fb42
kishart <amoralyse21@gmail.com>
2023-11-30 10:10:12 +0800

    align na wa nagkadimao

--- resources/views/admin/notification.blade.php
+++ resources/views/admin/notification.blade.php
@@ -1,45 +1,42 @@
 @extends('layouts.admin-n')
 @section('content')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 
-    <style>
-        /* CSS for card form */
-    body {
-      font-family: Arial, sans-serif;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      margin: 0;
-      background-color: #f4f4f4;
-    }
-    
-    .card {
-      width: 70%;
-      margin-bottom: 20%;
-      padding: 20px;
-      border-radius: 8px;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-      background-color: #fff;
-    }
-    
-    form {
-      display: flex;
-      flex-direction: column;
-    }
-    
-    </style>
-    @foreach ($data as $appoint)
-    <div class="card">
-        <form>
-            <p>Name: <span>{{$appoint->name}}</span></p>
-            <p>Email: <span>{{$appoint->email}}</span> </p>
-            <p>Phone Number: <span>{{$appoint->phone}}</span></p>
-            <p>Message: <span>{{$appoint->message}}</span></p>
-        </form>
-
-      </div>
-
-      @endforeach
-   
+        <style>
+            .card {
+                width: 70%;
+                margin: 0 auto;
+                padding: 10px;
+                border-radius: 8px;
+                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                background-color: #fff;
+                text-align: center;
+            }
+        
+            form {
+                display: flex;
+                flex-direction: column;
+            }
+        
+            form p {
+                margin: 5px 0;
+            }
+        
+            form p span {
+                font-weight: bold;
+            }
+        </style>
+        
+        @foreach ($data as $appoint)
+            <div class="card">
+                <form>
+                    <p>Name: <span>{{ $appoint->name }}</span></p>
+                    <p>Email: <span>{{ $appoint->email }}</span></p>
+                    <p>Phone Number: <span>{{ $appoint->phone }}</span></p>
+                    <p>Message: <span>{{ $appoint->message }}</span></p>
+                </form>
+            </div>
+        @endforeach
+        
--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -1,74 +1,71 @@
 @extends('layouts.navbar')
 @section('content')
-<link rel="stylesheet" href="{{url('css/contact.css')}}">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Chewy&family=Inter&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ url('css/contact.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Chewy&family=Inter&display=swap" rel="stylesheet">
     <!--bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container" style="margin-top:20px">
-        <div class="row">
-            <div class="col-md-12">
-                <h2 class="title">Contact Us</h2>
-                @if(Session::has('success'))
-                <div class="alert alert-success" role="alert">
-                    {{Session::get('success')}}
-                </div>
-                @endif
-                <form method="post" action="{{url('save-contact')}}">
-                    @csrf
-                    <div class="md-3">
-                        <label class="form-label">Name:</label>
-                        <input type="text" class="form-control" name="name" 
-                        value="{{old('name')}}">
-                        @error('name')
-                        <div class="alert alert-danger" role="alert">
-                            {{($message)}}
-                        </div>
-                        @enderror
-                        
-                    </div>
-                    <div class="md-3">
-                        <label class="form-label">Email:</label>
-                        <input type="text" class="form-control" name="email" 
-                         value="{{old('email')}}">
-                        @error('email')
-                        <div class="alert alert-danger" role="alert">
-                            {{($message)}}
-                        </div>
-                        @enderror
-                    </div>
-                    <div class="md-3">
-                        <label class="form-label">Phone:</label>
-                        <input type="text" class="form-control" name="phone" 
-                        value="{{old('phone')}}">
-                        @error('phone')
-                        <div class="alert alert-danger" role="alert">
-                            {{($message)}}
-                        </div>
-                        @enderror
-                    </div>
-                   
-                    <div class="md-3">
-                        <label class="form-label">Message:</label>
-                        <textarea class="form-control" name="message" 
-                       > {{old('message')}}</textarea>
-                            @error('message')
-                            <div class="alert alert-danger" role="alert">
-                                {{($message)}}
-                            </div>
-                            @enderror 
-                    </div><br>
-                    <button type="submit"class="button-28" role="button">Submit</button>
-                </form>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
 
+    <body>
+        <div class="container" style="margin-top:20px">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="title">Contact Us</h2>
+                    @if (Session::has('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('success') }}
+                        </div>
+                    @endif
+                    <form method="post" action="{{ url('save-contact') }}">
+                        @csrf
+                        <div class="md-3">
+                            <label class="form-label">Name:</label>
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                            @error('name')
+                                <div class="alert alert-danger" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+
+                        </div>
+                        <div class="md-3">
+                            <label class="form-label">Email:</label>
+                            <input type="text" class="form-control" name="email" value="{{ old('email') }}">
+                            @error('email')
+                                <div class="alert alert-danger" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="md-3">
+                            <label class="form-label">Phone:</label>
+                            <input type="text" class="form-control" name="phone" value="{{ old('phone') }}">
+                            @error('phone')
+                                <div class="alert alert-danger" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+
+                        <div class="md-3">
+                            <label class="form-label">Message:</label>
+                            <textarea class="form-control" name="message"> {{ old('message') }}</textarea>
+                            @error('message')
+                                <div class="alert alert-danger" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div><br>
+                        <button type="submit"class="button-28" role="button">Submit</button>
+                    </form>
+
+                </div>
             </div>
         </div>
-    </div>
 
 
-</body>
-
+    </body>
 @endsection
