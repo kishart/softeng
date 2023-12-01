42019d5f98860b064390aa6c031d4e92f31d5707
kishart <amoralyse21@gmail.com>
2023-12-01 21:46:46 +0800

    home page done

--- resources/views/admin/notification.blade.php
+++ resources/views/admin/notification.blade.php
@@ -5,13 +5,14 @@
 
 
         <style>
+ 
             .card {
                 width: 70%;
                 margin: 30 auto;
                 padding: 10px;
                 border-radius: 8px;
                 box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                background-color: #fff;
+                background-color: #fffbfa;
                 text-align: center;
             }
         
@@ -29,10 +30,16 @@
             form p span {
                 font-weight: bold;
             }
+            h1.text-center{
+                color: #854836;
+            }
         </style>
         
+        <h1 class="text-center ">Notification</h1>
         @foreach ($data as $appoint)
+        
             <div class="card">
+                
                 <form>
                     <p>Name: <span>{{ $appoint->name }}</span></p>
                     <p>Email: <span>{{ $appoint->email }}</span></p>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -9,7 +9,7 @@
 	  <div class="hero-section">
 		<div class="gallery">
 			<div class="image-section">
-				<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/368468781_735911958547578_1781721343468589615_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ARvgJtzJvekAX9IErca&_nc_ht=scontent.fozc1-1.fna&oh=00_AfCBzidT7dkaaDuct87ApPu0bcm0nvgkbST1wMNJlz7UcA&oe=655CAFD4" alt="birthday">
+				<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/371095375_737125898426184_7851624581153449435_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=a73e89&_nc_ohc=knn43CB9EMMAX8Hhbtx&_nc_ht=scontent.fozc1-1.fna&oh=00_AfDMDEElUt3uoFfbQAJTMD3i5mQXZq7syFq0zcdtD6CQ4g&oe=656F97A5" alt="birthday">
 			  </div>
 		  <div class="caption">
 			<h2>Graduation</h2>
@@ -25,7 +25,7 @@
 		</div>
 		<div class="gallery">
 		  <div class="image-section">
-			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/368468781_735911958547578_1781721343468589615_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ARvgJtzJvekAX9IErca&_nc_ht=scontent.fozc1-1.fna&oh=00_AfCBzidT7dkaaDuct87ApPu0bcm0nvgkbST1wMNJlz7UcA&oe=655CAFD4" alt="birthday">
+			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/302735117_480014250804018_7829380734029611629_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=a73e89&_nc_ohc=P_z6NYIYo1IAX-pgP4i&_nc_ht=scontent.fozc1-1.fna&oh=00_AfBAK-0_g8HRzC5uLRKSwUjunlj_VLPoCR-jQkllZv0TRg&oe=656F27EC" alt="birthday">
 		  </div>
 		  <div class="caption">
 			<h2>Family Picture</h2>
@@ -42,7 +42,7 @@
 	<div class="hero-section">
 		<div class="gallery">
 		  <div class="image-section">
-			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/368468781_735911958547578_1781721343468589615_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ARvgJtzJvekAX9IErca&_nc_ht=scontent.fozc1-1.fna&oh=00_AfCBzidT7dkaaDuct87ApPu0bcm0nvgkbST1wMNJlz7UcA&oe=655CAFD4" alt="birthday">
+			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/338185886_208510001824028_7484914279304622508_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a73e89&_nc_ohc=R4QuZqvMdooAX9S6-Rq&_nc_ht=scontent.fozc1-1.fna&oh=00_AfDCbwUhsxgpR7v_L_jFJs7JpunhFimlYOZKW44GmW62gA&oe=656EA34F" alt="birthday">
 		  </div>
 		  <div class="caption">
 			<h2>Pre Debu Shoot</h2>
@@ -50,14 +50,14 @@
 		</div>
 		<div class="gallery">
 		  <div class="image-section">
-			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/368468781_735911958547578_1781721343468589615_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ARvgJtzJvekAX9IErca&_nc_ht=scontent.fozc1-1.fna&oh=00_AfCBzidT7dkaaDuct87ApPu0bcm0nvgkbST1wMNJlz7UcA&oe=655CAFD4" alt="birthday">
+			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/335408141_3019647438329380_3783436486547619293_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a73e89&_nc_ohc=t9NwoylpWGcAX8BDiNV&_nc_ht=scontent.fozc1-1.fna&oh=00_AfDBtoxpZZcOupPa232_vtInbNbG2iD4e8TezxtUJDGjJA&oe=656ECC62" alt="wedding">
 		  </div>
 		  <div class="caption">
-			<h2>Birthday</h2>
+			<h2>Wedding</h2>
 		  </div>
 		</div><div class="gallery">
 		  <div class="image-section">
-			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/368468781_735911958547578_1781721343468589615_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ARvgJtzJvekAX9IErca&_nc_ht=scontent.fozc1-1.fna&oh=00_AfCBzidT7dkaaDuct87ApPu0bcm0nvgkbST1wMNJlz7UcA&oe=655CAFD4" alt="birthday">
+			<img src="https://scontent.fozc1-1.fna.fbcdn.net/v/t39.30808-6/387841732_766148935523880_5806313819820879875_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a73e89&_nc_ohc=uLNdIdlZvGgAX_2BpeL&_nc_ht=scontent.fozc1-1.fna&oh=00_AfC3F1nmaE5ZuLuhucQhLWiZpC29OxG-tu0cROD_K_yXdA&oe=656F24DE" alt="birthday">
 		  </div>
 		  <div class="caption">
 			<h2>Acquintance Party</h2>
